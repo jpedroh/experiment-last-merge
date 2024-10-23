94bef7916302543341c0500eac79d39c8981c650
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-10-23 11:54:44 -0300

    feat: adjusts

--- start-experiment.sh
+++ start-experiment.sh
@@ -1,6 +1,9 @@
 EXPERIMENT_NAME=$(date +"%Y_%m_%d_%H_%M")
 
+EXECUTIONS_FOLDER=$PWD/executions/$EXPERIMENT_NAME
+OUTPUT_FOLDER=$EXECUTIONS_FOLDER/output
+
 docker build -t experiment .
 mkdir -p output
-docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g) -v $PWD/clonedRepositories:/usr/src/app/clonedRepositories -v $PWD/output/$EXPERIMENT_NAME:/usr/src/app/output -v $PWD/projects.csv:/usr/src/app/projects.csv -v $PWD/commits.csv:/usr/src/app/commits.csv --name=generic_merge_experiment_$EXPERIMENT_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads 1 --injector=injectors.GenericMergeModule --extension=".java" --log-level=TRACE --keep-projects --since=17/07/2010 --until=17/07/2024 /usr/src/app/projects.csv
+echo docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g) -v $EXECUTIONS_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/projects.csv:/usr/src/app/projects.csv -v $PWD/commits.csv:/usr/src/app/commits.csv --name=generic_merge_experiment_$EXPERIMENT_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads 1 --injector=injectors.GenericMergeModule --extension=".java" --log-level=TRACE --keep-projects /usr/src/app/projects.csv
 echo Experiment is running on generic_merge_experiment_${EXPERIMENT_NAME}
