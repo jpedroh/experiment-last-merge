5d2429dc19161fbe91853c7f7ea66d28b83c2a06
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-10-23 11:57:02 -0300

    fix issue

--- start-experiment.sh
+++ start-experiment.sh
@@ -4,6 +4,6 @@ EXECUTIONS_FOLDER=$PWD/executions/$EXPERIMENT_NAME
 OUTPUT_FOLDER=$EXECUTIONS_FOLDER/output
 
 docker build --pull -t experiment .
-mkdir -p output
-echo docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g) -v $EXECUTIONS_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/projects.csv:/usr/src/app/projects.csv -v $PWD/commits.csv:/usr/src/app/commits.csv --name=generic_merge_experiment_$EXPERIMENT_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads 1 --injector=injectors.GenericMergeModule --extension=".java" --log-level=TRACE --keep-projects /usr/src/app/projects.csv
+mkdir -p $EXECUTIONS_FOLDER
+docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g) -v $EXECUTIONS_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/projects.csv:/usr/src/app/projects.csv -v $PWD/commits.csv:/usr/src/app/commits.csv --name=generic_merge_experiment_$EXPERIMENT_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads 1 --injector=injectors.GenericMergeModule --extension=".java" --log-level=TRACE --keep-projects /usr/src/app/projects.csv
 echo Experiment is running on generic_merge_experiment_${EXPERIMENT_NAME}
