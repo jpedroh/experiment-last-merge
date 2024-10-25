d6be884d0d61ca163e8fb827304692fb6d866b71
Joao Duarte <joao.pedro.hsd@gmail.com>
2024-10-24 21:34:55 -0300

    feat: copy files

--- start-experiment.sh
+++ start-experiment.sh
@@ -5,5 +5,7 @@ OUTPUT_FOLDER=$EXECUTIONS_FOLDER/output
 
 docker build --pull -t experiment .
 mkdir -p $EXECUTIONS_FOLDER
+cp projects.csv $OUTPUT_FOLDER
+cp commits.csv $OUTPUT_FOLDER
 docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g) -v $EXECUTIONS_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/projects.csv:/usr/src/app/projects.csv -v $PWD/commits.csv:/usr/src/app/commits.csv --name=generic_merge_experiment_$EXPERIMENT_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads 1 --injector=injectors.GenericMergeModule --extension=".java" --log-level=TRACE --keep-projects --include-pull-request-branches /usr/src/app/projects.csv
 echo Experiment is running on generic_merge_experiment_${EXPERIMENT_NAME}
