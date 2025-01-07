70a6f09812ea3bf5fe02c67adbff1cb2087f2f86
Joao Duarte <joao.pedro.hsd@gmail.com>
2025-01-06 23:40:14 -0300

    feat: update output folder

--- start-experiment.sh
+++ start-experiment.sh
@@ -12,7 +12,7 @@ else
 fi
 
 docker build --pull -t experiment .
-mkdir -p $EXECUTION_FOLDER
+mkdir -p $OUTPUT_FOLDER
 cp input/projects.csv $OUTPUT_FOLDER/projects.csv
 cp input/commits.csv $OUTPUT_FOLDER/commits.csv
 docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g)  -v $EXECUTION_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/input/projects.csv:/usr/src/app/projects.csv -v $PWD/input/commits.csv:/usr/src/app/commits.csv --name=$CONTAINER_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads $THREADS --injector=injectors.GenericMergeModule --access-key=${GITHUB_ACCESS_KEY} --extension=".java" --log-level=TRACE --keep-projects --project-commit-hashes-file=commits.csv /usr/src/app/projects.csv
