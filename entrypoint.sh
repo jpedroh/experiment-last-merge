4dcae2c4e96664c404231bdd458879e8c85ecf09
Joao Duarte <joao.pedro.hsd@gmail.com>
2026-06-11 19:16:22 -0300

    Switch diff3 image to Temurin

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,12 @@ HOST_USER_ID=${HOST_USER_ID:-9001}
 HOST_GROUP_ID=${HOST_GROUP_ID:-9001}
 
 # Create a user and group with the same IDs as the host user
-groupadd -g $HOST_GROUP_ID mygroup
-useradd -u $HOST_USER_ID -g mygroup -m myuser
+if ! getent group mygroup >/dev/null 2>&1; then
+    groupadd -g "$HOST_GROUP_ID" mygroup
+fi
+if ! id -u myuser >/dev/null 2>&1; then
+    useradd -u "$HOST_USER_ID" -g mygroup -m myuser
+fi
 
 # Change ownership of the mounted directory
 chown -R myuser:mygroup /usr/src/app
