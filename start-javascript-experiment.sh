de71e83893e3d8ab22d9bad71110b62a85d1ef68
Joao Duarte <joao.pedro.hsd@gmail.com>
2026-06-11 19:04:54 -0300

    Split dockerfile

--- start-javascript-experiment.sh
+++ start-javascript-experiment.sh
@@ -13,9 +13,9 @@ else
     CONTAINER_NAME=generic_merge_experiment_javascript_${EXPERIMENT_NAME}_${CONTAINER_NAME_SUFFIX}
 fi
 
-docker build --pull -t experiment .
+docker build --pull -f Dockerfile.diff3 -t experiment-javascript .
 mkdir -p $OUTPUT_FOLDER
 cp input/javascript/projects.csv $OUTPUT_FOLDER/projects.csv
 cp input/javascript/commits.csv $OUTPUT_FOLDER/commits.csv
-docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g)  -v $EXECUTION_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/input/javascript/projects.csv:/usr/src/app/projects.csv -v $PWD/input/javascript/commits.csv:/usr/src/app/commits.csv --name=$CONTAINER_NAME experiment ./tools/miningframework/install/miningframework/bin/miningframework --threads $THREADS --injector=injectors.GenericMergeDiff3Module --extension=".js" --log-level=TRACE --keep-projects --project-commit-hashes-file=commits.csv /usr/src/app/projects.csv
+docker run -d -e HOST_USER_ID=$(id -u) -e HOST_GROUP_ID=$(id -g)  -v $EXECUTION_FOLDER/clonedRepositories:/usr/src/app/clonedRepositories -v $OUTPUT_FOLDER:/usr/src/app/output -v $PWD/input/javascript/projects.csv:/usr/src/app/projects.csv -v $PWD/input/javascript/commits.csv:/usr/src/app/commits.csv --name=$CONTAINER_NAME experiment-javascript ./tools/miningframework/install/miningframework/bin/miningframework --threads $THREADS --injector=injectors.GenericMergeDiff3Module --extension=".js" --log-level=TRACE --keep-projects --project-commit-hashes-file=commits.csv /usr/src/app/projects.csv
 echo Experiment is running on $CONTAINER_NAME
